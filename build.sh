ac5248f832d49d6c00fc0df74db312bb7d8d4038
botmaster2020 <66076362+botmaster2020@users.noreply.github.com>
2022-10-03 17:45:26 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 echo "Building..."
 echo "The current branch is ${CI_COMMIT_BRANCH}"
 echo "The current commit hash is ${CI_COMMIT_SHA}"
-docker run -d nginx
+docker run -d --name=jjj nginx
 echo "Building done."
